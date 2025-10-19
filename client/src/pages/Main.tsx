2513f33c85db24eb8ed41c62dc39b84101767416
Liv <olivianwright11@gmail.com>
2025-10-19 02:37:37 -0500

    spinner

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -1,12 +1,16 @@
 import styles from "./Main.module.css"
 import mascot from "../assets/mascot.png"
+import Spinner from "../components/Spinner";
+import React, { useState} from "react";
 // import { useNavigate } from "react-router"
 
 const Main = () => {
-
+    const [loading, setLoading] = useState(false);
     // const navigate = useNavigate();
 
    const onClick = () => { chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        setLoading(true);
+
         if (tabs[0]?.id) {
         chrome.tabs.sendMessage(
             tabs[0].id,
@@ -14,7 +18,10 @@ const Main = () => {
             async (response) => {
             if (chrome.runtime.lastError) {
                 console.error(chrome.runtime.lastError.message);
-            } else {
+                setLoading(false);
+                return;
+            } 
+            try {
                 const html = response.html;
                 console.log(html);
                 const geminiResponse = await fetch(`http://localhost:3000/api/product_information?url=${encodeURIComponent(tabs[0].url!)}`,
@@ -29,6 +36,10 @@ const Main = () => {
                 )
 
                 console.log(await geminiResponse.json());
+            } catch (err) {
+                console.error(err);
+            } finally {
+                setLoading(false);
             }
             }
         );
@@ -39,7 +50,11 @@ const Main = () => {
     <main>
         <img id={styles['mascot-image']} src={mascot}/>
         <header>Vesta</header>
-        <button onClick={onClick}>Scan Now</button>
+        {loading ? (
+            <Spinner />
+        ) : (
+            <button onClick={onClick}>SCAN</button>
+        )}
     </main>
   )
 }
